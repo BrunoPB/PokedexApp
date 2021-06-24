1f99e94384643d105c059f885208546c659094ef
BrunoPB <brunopenabaeta@outlook.com>
2021-06-24 19:52:48 -0300

    Setting Up Workspace

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PokedexApp
 Simple Pokedex Application to study databases connection and http request response. Using: MySQL, Java, Maven, Spark, Angular
+Starting on June, 24, 2021
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './material.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
