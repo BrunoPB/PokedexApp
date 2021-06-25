d35cd7d72d0ddec53cfb6a2722d15f8a00368e46
BrunoPB <bruno.bax.bpb@gmail.com>
2021-06-25 11:05:32 -0300

    Criadas as classes das entidades

--- backend/src/main/java/app/entities/Relacionamento.java
+++ backend/src/main/java/app/entities/Relacionamento.java
@@ -1,5 +1,34 @@
 package app.entities;
 
 public class Relacionamento {
+    private int Usuario_ID;
+    private int Pokemon_ID;
 
+    // Getters and Setter
+    public int getUsuario_ID() {
+        return this.Usuario_ID;
+    }
+
+    public void setUsuario_ID(int Usuario_ID) {
+        this.Usuario_ID = Usuario_ID;
+    }
+
+    public int getPokemon_ID() {
+        return this.Pokemon_ID;
+    }
+
+    public void setPokemon_ID(int Pokemon_ID) {
+        this.Pokemon_ID = Pokemon_ID;
+    }
+
+    // Constructors
+    public Relacionamento(int usuario_id, int pokemon_id) {
+        this.Usuario_ID = usuario_id;
+        this.Pokemon_ID = pokemon_id;
+    }
+
+    public Relacionamento(Relacionamento relacionamento) {
+        this.Usuario_ID = relacionamento.Usuario_ID;
+        this.Pokemon_ID = relacionamento.Pokemon_ID;
+    }
 }
