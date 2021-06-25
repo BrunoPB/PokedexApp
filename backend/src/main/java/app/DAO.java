578153511448522e58c8f603f7c661a86d4118a8
BrunoPB <bruno.bax.bpb@gmail.com>
2021-06-25 00:59:56 -0300

    Criada a classe DAO, ja conectada com o banco

--- backend/src/main/java/app/DAO.java
+++ backend/src/main/java/app/DAO.java
@@ -1,5 +1,30 @@
 package app;
 
-public class DAO {
+import java.sql.*;
 
+public class DAO {
+    private String driver = "com.mysql.cj.jdbc.Driver";
+    private String porta = "3306";
+    private String database = "Pokedex";
+    private String user = "brunosql";
+    private String senha = "Acessosql#Dex";
+    private Connection conexao = null;
+
+    public DAO() {
+        try {
+            Class.forName(driver);
+        } catch (Exception e) {
+            System.out.println("Falha no carregamento do Driver: " + e.getMessage());
+        }
+        try {
+            String url = ("jdbc:mysql://localhost:" + porta + "/" + database);
+            conexao = DriverManager.getConnection(url, user, senha);
+        } catch (Exception e) {
+            System.out.println("Falha na conexao com o Banco de Dados: " + e.getMessage());
+        }
+    }
+
+    public Connection getConexao() {
+        return this.conexao;
+    }
 }
--- backend/src/main/java/app/Main.java
+++ backend/src/main/java/app/Main.java
@@ -1,7 +1,11 @@
 package app;
 
+import spark.Spark.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        DAO dao = new DAO();
+
+        System.out.println(dao.getConexao());
     }
 }
