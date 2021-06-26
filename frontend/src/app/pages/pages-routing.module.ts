56779370326e1cc2be327306420658d58e9c756d
BrunoPB <bruno.bax.bpb@gmail.com>
2021-06-26 03:02:50 -0300

    Criado o HTML e CSS das telas profile e addpokemon

--- frontend/src/app/pages/pages-routing.module.ts
+++ frontend/src/app/pages/pages-routing.module.ts
@@ -1,33 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddPokemonComponent } from './addpokemon/addpokemon.component';
 import { PagesComponent } from './pages.component';
-import { PokedexViewComponent } from './pokedexview/pokedexview.component';
-import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PagesComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'pokedex',
-        pathMatch: 'full',
-      },
-      {
-        path: 'pokedex',
-        component: PokedexViewComponent,
-      },
-      {
-        path: 'profile',
-        component: ProfileComponent,
-      },
-      {
-        path: 'add',
-        component: AddPokemonComponent,
-      },
-    ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
   },
 ];
 
